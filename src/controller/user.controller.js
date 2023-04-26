62a3f26faef9a67dfee0cc9b4dd1ba394adb9683
wanglongkai <13699283312@163.com>
2023-04-26 14:44:49 +0800

    颁发token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,7 @@
-const { createUser } = require('../service/user.service');
+const jwt = require('jsonwebtoken');
+const { createUser, getUerInfo } = require('../service/user.service');
 const { userRegisterError } = require('../constant/err.type');
+const { JWT_SECRET } = require('../config/config.default')
 class UserController {
     async register(ctx, next) {
         const { user_name, password } = ctx.request.body;
@@ -20,7 +22,21 @@ class UserController {
     }
 
     async login(ctx, next) {
-        ctx.body = `login-${ctx.request.body}`
+        const { user_name } = ctx.request.body
+        // 1. 获取用户信息(在token的payload中, 记录id, user_name, is_admin)
+        try {
+            // 从返回结果对象中剔除password属性, 将剩下的属性放到res对象
+            const { password, ...res } = await getUerInfo({ user_name })
+            ctx.body = {
+                code: 0,
+                message: '用户登录成功',
+                result: {
+                    token: jwt.sign(res, JWT_SECRET, { expiresIn: '1d' }),
+                },
+            }
+        } catch (err) {
+            console.error('用户登录失败', err)
+        }
     }
 }
 
