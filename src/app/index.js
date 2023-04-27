59d17f485cf11526ed8437a5fec410176756a2c3
wanglongkai <13699283312@163.com>
2023-04-27 16:48:27 +0800

    对文件上传缓存路径理解修正

--- src/app/index.js
+++ src/app/index.js
@@ -21,7 +21,7 @@ App.use(
 App.use(koaBody({
     multipart: true, // 支持文件上传
     formidable: {
-        uploadDir: path.resolve(__dirname, '../upload'), // 文件上传保存路径
+        uploadDir: path.resolve(__dirname, '../upload'), // 文件上传缓存路径，业务真正执行分件管理后，需要删除
         keepExtensions: true, // 保留文件后缀名
         filename: (name, ext) => { // 重命名保存名称
             return `${new Date().getTime()}-${name}${ext}`
